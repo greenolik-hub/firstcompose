a2b9a6dfbdd1824215e214b1911f04557fd0ebb6
egor <egorrcho@rambler.ru>
2026-02-01 16:05:44 +0300

    Initial commit

---
+++ backend/backend.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+
+@app.route("/message", methods=["GET"])
+def message_get():
+    return jsonify({
+        "status": "Hi from container! nigga"
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
